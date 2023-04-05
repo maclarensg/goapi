9cc3700f2139dcc64daf88cc38ffaed0a3c80a40
Gavin Yap <gavin.yap@ifun.com.sg>
2023-04-05 10:43:59 +0800

    save

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	docs "github.com/maclarensg/goapi/docs"
 
@@ -25,6 +27,17 @@ func Helloworld(g *gin.Context) {
 	g.JSON(http.StatusOK, "helloworld")
 }
 
+type RootResponse struct {
+	Version    string `json:"version"`
+	Date       int64  `json:"date"`
+	Kubernetes bool   `json:"kubernetes"`
+}
+
+func isRunningInKubernetes() bool {
+	_, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount")
+	return err == nil
+}
+
 func main() {
 	r := gin.Default()
 	docs.SwaggerInfo.BasePath = "/api/v1"
@@ -35,6 +48,26 @@ func main() {
 			eg.GET("/helloworld", Helloworld)
 		}
 	}
+
+	// @Summary Get root information
+	// @Description Get application version, current date (UNIX epoch), and Kubernetes status.
+	// @ID get-root
+	// @Produce json
+	// @Success 200 {object} RootResponse
+	// @Router / [get]
+	r.GET("/", func(c *gin.Context) {
+		resp := RootResponse{
+			Version:    "0.1.0",
+			Date:       time.Now().Unix(),
+			Kubernetes: isRunningInKubernetes(),
+		}
+		c.JSON(http.StatusOK, resp)
+	})
+
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.GET("/swagger", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/swagger/index.html")
+	})
 	r.Run(":8080")
 }
